4e244e8d25765e6faa9419bbd89f6e4acf018a07
Majd Alkawaas <mma250@mail.aub.edu>
2022-07-27 21:40:47 +0300

    Updates to the app
    - Added exit command for either sides the server and the client.

--- server.c
+++ server.c
@@ -12,47 +12,73 @@ void error(const char *msg)
     exit(1);
 }
 
+void check_exit(char* buffer)
+{
+    if(strcmp(buffer, "exit") == 0)
+    {    
+        exit(1);
+    }
+}
+
 int main(int argc, char *argv[])
 {
 
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     int sockfd, newsockfd, portno,n;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     socklen_t clilen;
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0)
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd,
-           (struct sockaddr *) &cli_addr, &clilen);
-     if (newsockfd < 0)
-         error("ERROR on accept");
-     while (strcmp(buffer,"exit\n")!=0) {
+    if (argc < 2)
+    {
+        fprintf(stderr, "ERROR, no port provided\n");
+        exit(1);
+    }
+    int sockfd, newsockfd, portno, n;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    socklen_t clilen;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-       bzero(buffer,256);
-       n = read(newsockfd,buffer,256);
-       if (n < 0)
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+    
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        error("ERROR on binding");
+    
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+    
+    if (newsockfd < 0)
+        error("ERROR on accept");
+    else
+		printf("You are connected now client\n");
+    
+    while (1)
+    {
+
+        bzero(buffer, 256);
+        n = read(newsockfd, buffer, 256);
+        buffer[strcspn(buffer, "\n")] = 0;
+        check_exit(buffer);
+        if (n < 0)
             error("ERROR reading from socket");
-       printf("Client : %s\n",buffer);
-       bzero(buffer,256);
-       fgets(buffer,256,stdin);
-       n = write(newsockfd,buffer,strlen(buffer));
-       if (n < 0)
+
+        printf("Client : %s\n", buffer);
+        bzero(buffer, 256);
+        printf("Server: ");
+        fgets(buffer, 256, stdin);
+        n = write(newsockfd, buffer, strlen(buffer));
+        buffer[strcspn(buffer, "\n")] = 0;
+        check_exit(buffer);
+        if (n < 0)
             error("ERROR writing to socket");
-     } /* end of while */
-     close(newsockfd);
-     close(sockfd);
-     return 0; /* we never get here */
+    } /* end of while */
+
+
+    close(newsockfd);
+    close(sockfd);
+    return 0; /* we never get here */
 }
